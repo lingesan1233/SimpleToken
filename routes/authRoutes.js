c4689562215ec1523069f42d064c9355ac1ce103
lingesan <sglingesan@gmail.com>
2025-04-26 17:02:47 +0530

    complited

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { register, login, getUser } = require('../controllers/authController');
+const protect = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/me', protect, getUser);
+
+module.exports = router;
